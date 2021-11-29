d9300c3096508a8ab16e36e121cc6ee96f288e82
Daniel Cerezuela Dominguez <dcerdom701@g.educaand.es>
2021-11-29 13:52:11 +0100

    funcion del rectangulo

---
+++ clases/rectangulo.php
@@ -0,0 +1,15 @@
+<?php
+
+function areaRectangulo($base, $altura){
+    $area = $base*$altura;
+    return $area;
+}
+
+function perimetroRectangulo($base, $altura){
+    $perimetro = $base*2 + $altura*2;
+    return $perimetro;
+}
+
+
+
+?>
