f8ae8b3bfae91b030ed62070ef561eeaddff92ce
Daniel Cerezuela Dominguez <dcerdom701@g.educaand.es>
2021-11-29 13:32:12 +0100

    creada las funciones para pentagono

---
+++ clases/pentagono.php
@@ -0,0 +1,13 @@
+<?php
+
+    function areaPentagono($base, $ap){
+        $area = ($base*$ap*5)/2;
+        return $area;
+    }
+
+    function perimetroPentagono($base){
+        $perimetro=$base*5;
+        return $perimetro;
+    }
+
+?>
