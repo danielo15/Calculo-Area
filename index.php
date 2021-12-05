4b1a424e8f8fcdf3b6f06aa8a3fbcbe39d81a4f8
Jesus Jurado <jjurbar021@g.educaand.es>
2021-12-05 10:33:03 +0100

    Creación de index
    Creación del index.php de la aplicación web.

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<?php
+//Inicio codificación: JJB - 05/12/2021
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="" method="post">
+
+        <label>Triangulo</label>
+        <input type="number" name="base" placeholder="base">
+        <input type="number" name="altura" placeholder="altura">
+        <input type="submit" name="triangulo">
+        
+    </form>
+
+    <br>
+
+    <form action="" method="post">
+
+        <label>Circulo</label>
+        <input type="number" name="radio" placeholder="radio">
+        <input type="submit" name="circulo">
+        
+    </form>
+
+</body>
+</html>
